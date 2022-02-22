401597ce9e33bbd577f435527d149838d974a156
Raymond <raymondtremblay1@icloud.com>
2022-02-22 15:46:23 -0400

    build-2022-02-22

--- embedded_flipbooks/Anolis.R
+++ embedded_flipbooks/Anolis.R
@@ -1,1 +1,7 @@
 
+Anolis %>%  # pipe
+  select(WEIGHT, SVL) %>%  # Seleccionar solamente la siguientes columnas
+ filter(WEIGHT<4 & SVL>20)%>%
+  head()%>%            # solo muestra los primeros seis
+  gt()
+
