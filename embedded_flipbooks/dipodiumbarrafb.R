e294a2d5f5c2b9baa53ddf7f8db3dd18945a6870
RaymondLTremblay <raymondtremblay1@icloud.com>
2022-03-07 10:56:15 -0400

    G7 and G8

---
+++ embedded_flipbooks/dipodiumbarrafb.R
@@ -0,0 +1,10 @@
+DW %>%
+  drop_na(number_of_flowers)%>%
+  ggplot(aes(number_of_flowers))+
+  geom_bar(fill="red",color="white")+
+  xlab("Cantidad de Flores")+
+  ylab("Frecuencia")+
+  theme(axis.title=element_text(size=14,face="bold"))
+
+
+
