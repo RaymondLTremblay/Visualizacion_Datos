971238b5760b89f79b6fad8b707bb7d8be6f77cb
Raymond <raymondtremblay1@icloud.com>
2023-03-06 09:13:54 -0400

    2023-03-06_violin

--- embedded_flipbooks/dipodium-puntos.R
+++ embedded_flipbooks/dipodium-puntos.R
@@ -1,8 +1,1 @@
 
-dipodium %>%
-  select(number_of_fruits, species_name, tree_species)%>%
-  filter(species_name=="r")%>%   # r representa la especie de Dipodium roseum
-  ggplot(aes(number_of_fruits))+
-  geom_dotplot(stackratio=.5, fill="magenta", colour = "red")+
-  labs(x="Número de Frutos", y="Frecuencia")+
-  theme(axis.title=element_text(size=10,face="bold"))
