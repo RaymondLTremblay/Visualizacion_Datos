cbedff346a2d96be73b7e51a2720bc135940ce2b
Raymond <raymondtremblay1@icloud.com>
2023-04-26 16:11:24 -0400

    yml update

--- embedded_flipbooks/geom_point-count.R
+++
@@ -1,7 +0,0 @@
-Anolis %>%
-  select(WEIGHT, SVL) %>%
-  filter(WEIGHT<4 & SVL>20) %>%
-ggplot(aes(x=WEIGHT, y=SVL))+
-  geom_point()+
-    labs(title= "Figura. Relación de SVL y  el peso de Anolis en \n El Verde, Yunque, PR")+
-  ylab("Snout vent length")
